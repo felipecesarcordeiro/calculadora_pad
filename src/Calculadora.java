429ab35e7105f0db475acf9f02ac841b15472f9c
Felipe <felipecesarcordeiro@gmail.com>
2015-11-10 18:09:49 -0300

    versão MultiThread

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -7,61 +7,47 @@ import java.io.*;
  * github:felipecesarcordeiro e-mail:felipecesarcordeiro@gmail.com*/
 
 
-public class Calculadora 
+public class Calculadora implements Runnable
 {
-		int op1, op2;
-		char operador;
-		float resultado;
+	private Operacao operacao;
+	private Operacao vaux;
 	
-		public double calcula(int op1, int op2, char operador) 
-		{
-			double result = 0;
-			this.op1 = op1;
-			this.op2 = op2;
-			this.operador = operador;
-			
-			switch (operador) 
-			{
-				case '+':
-					{
-						result = op1 + op2;
-						break;
-					}
-				case '-':
-					{
-						result = op1 - op2;
-						break;
-					}
-				case '/':
-					{
-						result = op1 / op2;
-						break;
-					}
-				case '*':
-					{
-						result = op1* op2;
-						break;
-					}
-			}
-			
-			try 
-			{
-				PrintWriter log = new PrintWriter(new FileWriter("log.txt",true), true);
-				log.write(this.dados());
-				log.close();
-			} 
-			catch (IOException e) 
-			{
-				e.printStackTrace();
-				
-			}
-			return result;
-		}
-	
-		public String dados() 
-		{
-	
-	
-			return (String.valueOf(op1) + operador + String.valueOf(op2));
-		}
+	public Calculadora(Operacao opera)
+	{
+		this.operacao = opera;
 	}
+	
+	@Override
+	public void run()
+	{
+		while (true)
+		{
+			vaux = operacao.getOk();
+			int res; 
+				
+			if(vaux.getOperador() == '+')
+			{
+				res = vaux.somar();
+			}
+			else 
+				if (vaux.getOperador() == '-')
+				{
+					res = vaux.subtrair();			
+				} 
+				else 
+					if(vaux.getOperador() == '*') 
+					{
+						res = vaux.multiplicar();			
+					} 
+					else 
+					{
+						res = vaux.dividir();
+					}
+				
+			System.out.printf("Resultado: %d\n", res);
+		}
+	
+	}
+
+	
+}
